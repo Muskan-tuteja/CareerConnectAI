e93beb8e64b83060abd84d631abd5b00867c89ec
muskan <muskantuteja714@gmail.com>
2026-06-04 22:45:45 +0530

    Add files

--- userservice/src/main/java/com/CareerConnect/userservice/dto/RegisterRequest.java
+++ userservice/src/main/java/com/CareerConnect/userservice/dto/RegisterRequest.java
@@ -8,10 +8,11 @@ import lombok.Data;
 
 @Data
 public class RegisterRequest {
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
+
     private String firstname;
     private String lastname;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
     @NotBlank(message = "Password is requried")
     @Size(min = 6, message = "Password must have atLeast 6 Character")
