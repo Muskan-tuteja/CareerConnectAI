1d72352b4413dfae256e67325ac7d3216abd3ff0
Muskan-tuteja <muskantuteja714@gmail.com>
2026-07-03 17:17:40 +0530

    update1

--- userservice/src/main/java/com/CareerConnect/userservice/service/UserService.java
+++ userservice/src/main/java/com/CareerConnect/userservice/service/UserService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-// @AllArgsConstructor
+@AllArgsConstructor
 @Slf4j
 public class UserService {
 
